8da5103fad45e9fd15049e3b5acb42d56de6325a
Akash-IISC <148427692+Akash-IISC@users.noreply.github.com>
2024-04-07 17:29:21 +0530

    Update train.py

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ X_scaled = scaler.fit_transform(X)
 #grid_search.fit(X_scaled, y)  # Missing values will be handled by the decision tree
 
 # Hyperparameter Tuning with GridSearchCV
-param_grid = {'C': [100000, 50000, 60000, 10000,5000, 3000, 4000, 2000, 1000, 500]}  # Values of 'C' to try
+param_grid = {'C': [0.001, 0.01, 0.1, 0.2, 0.5, 1, 1.5, 2]}  # Values of 'C' to try
 grid_search = GridSearchCV(LogisticRegression(penalty='l2', max_iter=10000), param_grid)
 grid_search.fit(X_scaled, y)
 
