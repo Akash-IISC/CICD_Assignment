936dbd88bdca5bb4e67c6376032aac8350fc9c64
Akash-IISC <148427692+Akash-IISC@users.noreply.github.com>
2024-04-07 13:28:26 +0530

    Update train.py with regularization with scaling for improving accuracy

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler #added standardscaler
 import pickle
 import numpy as np
 
@@ -9,8 +10,14 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
+# Feature Scaling
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
 #model = LogisticRegression().fit(X, y)
-model = LogisticRegression(max_iter=2000).fit(X, y)
+#model = LogisticRegression(max_iter=2000).fit(X, y)
+model = LogisticRegression(penalty='l2', C=1.0, max_iter=2000).fit(X_scaled, y) #regularization with scaling
+
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
