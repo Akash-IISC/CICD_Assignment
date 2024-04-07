526286dd5e01c9ead3764c570f09291fe6a06a6d
Akash-IISC <148427692+Akash-IISC@users.noreply.github.com>
2024-04-07 13:39:38 +0530

    Update train.py with grid search to find best regulariser C

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler #added standardscaler
+from sklearn.model_selection import GridSearchCV #added grid search
 import pickle
 import numpy as np
 
@@ -14,10 +15,23 @@ y = np.array([np.where(labels == x) for x in y]).flatten()
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
+# Hyperparameter Tuning with GridSearchCV
+param_grid = {'C': [0.5, 0.1, 0.01]}  # Values of 'C' to try
+grid_search = GridSearchCV(LogisticRegression(penalty='l2', max_iter=2000), param_grid)
+grid_search.fit(X_scaled, y)
+
+# Get the best model
+best_model = grid_search.best_estimator_
+print("Best parameters:", grid_search.best_params_)
+print("Best model:", best_model)
+
 #model = LogisticRegression().fit(X, y)
 #model = LogisticRegression(max_iter=2000).fit(X, y)
-model = LogisticRegression(penalty='l2', C=1.0, max_iter=2000).fit(X_scaled, y) #regularization with scaling
+#model = LogisticRegression(penalty='l2', C=1.0, max_iter=2000).fit(X_scaled, y) #regularization with scaling
 
-
+# Save the best model
 with open("model.pkl", 'wb') as f:
-    pickle.dump(model, f)
+    pickle.dump(best_model, f)
+
+#with open("model.pkl", 'wb') as f:
+ #   pickle.dump(model, f)
